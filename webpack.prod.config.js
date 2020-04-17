44c26f2bd2f13840c7c19a49004ed1ba4c2297bb
Leandro Almeida <vempari@gmail.com>
2020-04-17 16:39:42 -0300

    Adicionado config para produção

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -9,22 +9,24 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = validate(
   {
-    devtool: 'source-map',
-    entry: [
-      'react-hot-loader/patch',
-      'webpack-dev-server/client?http://localhost:3000',
-      'webpack/hot/only-dev-server',
-      path.join(__dirname, 'src', 'index')
-    ],
+    entry: path.join(__dirname, 'src', 'index'),
 
     output: {
       path: path.join(__dirname, 'dist'),
-      filename: '[name]-[hash].js',
-      publicPath: ''
+      filename: '[name]-[hash].js'
     },
 
     plugins: [
-      new webpack.HotModuleReplacementPlugin(),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: '"production"'
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: { warnings: false }
+      }),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.OccurrenceOrderPlugin(),
       new ExtractTextPlugin('[name]-[hash].css'),
       new HtmlPlugin({
         title: 'Github app',
@@ -49,7 +51,7 @@ module.exports = validate(
         test: /\.css$/,
         exclude: /node_modules/,
         include: /src/,
-        loaders: ['style', 'css']
+        loader: ExtractTextPlugin.extract('style', 'css')
       }]
     }
   }
