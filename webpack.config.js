6b8c38b382e685e7788392efd2d4bdc7497b1098
Leandro Almeida <vempari@gmail.com>
2020-04-20 15:42:41 -0300

    Add visualizador de dashboard ao webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const validate = require('webpack-validator')
 
 const HtmlPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const DashboardPlugin = require('webpack-dashboard/plugin')
 
 module.exports = validate(
   {
@@ -25,6 +26,7 @@ module.exports = validate(
 
     plugins: [
       new webpack.HotModuleReplacementPlugin(),
+      new DashboardPlugin(),
       new ExtractTextPlugin('[name]-[hash].css'),
       new HtmlPlugin({
         title: 'Github app',
