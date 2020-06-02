8e634d4b408328a89bbfa0b5b49504206b6e4f7f
Leandro Almeida <vempari@gmail.com>
2020-06-02 20:13:57 -0300

    adicionado componente de paginação com storybook

--- src/components/pagination/index.js
+++ src/components/pagination/index.js
@@ -1,10 +1,36 @@
 'use strict'
 
-import React from 'react'
+import React, { PropTypes } from 'react'
 import pagination from '../../utils/pagination'
+import Page from './page'
 
-const Pagination = ({ total, activePages }) => (
-  <div>Pagination</div>
+const Pagination = ({ total, activePage, pageLink, onClick }) => (
+  <ul>
+    {pagination({ total, activePage }).map((page, index) => (
+      <li
+        key={index}
+        style={activePage === page ? { color: '#f00' } : null}
+      >
+        <Page
+          page={page}
+          pageLink={pageLink.replace('%page%', page)}
+          onClick={onClick}
+        />
+      </li>
+    ))}
+  </ul>
 )
+
+Pagination.defaultProps = {
+  pageLink: '',
+  activePage: 1
+}
+
+Pagination.propTypes = {
+  total: PropTypes.number,
+  activePage: PropTypes.number,
+  pageLink: PropTypes.string,
+  onClick: PropTypes.func
+}
 
 export default Pagination
--- src/components/pagination/pagination.story.js
+++ src/components/pagination/pagination.story.js
@@ -7,5 +7,31 @@ import Pagination from './index'
 const stories = storiesOf('<Pagination />', module)
 
 stories.add('without props', () => (
-    <Pagination />
+  <Pagination />
+))
+
+stories.add('with total & active page', () => (
+  <Pagination
+    total={10}
+    activePage={5}
+  />
+))
+
+stories.add('with page link', () => (
+  <Pagination
+    total={20}
+    activePage={10}
+    pageLink='http://www.mypage.com/page/%page%'
+  />
+))
+
+stories.add('with callback', () => (
+  <Pagination
+    total={30}
+    activePage={15}
+    pageLink='http://www.mypage.com/page/%page%'
+    onClick={(page) => {
+      window.alert(page)
+    }}
+  />
 ))
